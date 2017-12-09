105084b2e32dd61746e3d25a30200d589b5b7fde
TheAHTOH96 <anton.vydolob96@gmail.com>
2017-12-09 19:17:32 +0200

    add find method

--- src/main/java/com/labcloud/prog1/repositories/ProductsRepositories.java
+++ src/main/java/com/labcloud/prog1/repositories/ProductsRepositories.java
@@ -6,16 +6,17 @@ import org.springframework.data.repository.query.Param;
 
 import java.math.BigInteger;
 import java.util.List;
+import java.util.Set;
 
 public interface ProductsRepositories extends MongoRepository<Products, String> {
 
-    List<Products> findByName(@Param("ProductName") String name);
-    List<Products> findByAmount(@Param("ProductByAmount") BigInteger amount);
-    List<Products> findByPrice(@Param("ProductByPrice") BigInteger price);
-    List<Products> findByAmountAfter(@Param("ProductByAmountAfter") BigInteger amount);
-    List<Products> findByAmountBefore(@Param("ProductByAmountBefore") BigInteger amount);
-    List<Products> findByPriceAfter(@Param("ProductByPriceAfter") BigInteger price);
-    List<Products> findByPriceBefore(@Param("ProductByPriceBefore") BigInteger price);
+    Set<Products> findByName(@Param("ProductName") String name);
+    Set<Products> findByAmount(@Param("ProductByAmount") BigInteger amount);
+    Set<Products> findByPrice(@Param("ProductByPrice") BigInteger price);
+    Set<Products> findByAmountLessThanEqual(@Param("ProductByAmountLessThanEqual") BigInteger amount);
+    Set<Products> findByAmountGreaterThanEqual(@Param("ProductByAmountGreaterThanEqual") BigInteger amount);
+    Set<Products> findByPriceLessThanEqual(@Param("ProductByPriceLessThanEqual") BigInteger price);
+    Set<Products> findByPriceGreaterThanEqual(@Param("ProductByPriceGreaterThanEqual") BigInteger price);
     //List<Products> findTopByAmount(@Param("ProductAmount") BigInteger amount);
     //List<Products> findByPriceLessThan(@Param("ProductPrice") BigInteger price);
     //List<Products> findByPriceBefore(@Param("ProductPriceBefore") BigInteger price);
